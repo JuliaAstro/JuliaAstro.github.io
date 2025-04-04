89b6c340cd31151a7c0b5cdf28ed5d5e3451dcc7
icweaver <weaveric@gmail.com>
2025-04-04 08:55:35 -0700

    update branches and switch deploy method

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ using LibGit2, Pkg, TOML, UUIDs, Downloads
 # That way, docs for all packages are browsable and searchable in one place!
 
 clonedir = ("--temp" in ARGS) ? mktempdir() : joinpath(@__DIR__, "clones")
-outpath = joinpath(@__DIR__, "build")
+outpath = joinpath(@__DIR__, "out")
 @info """
 Cloning packages into: $(clonedir)
 Building aggregate site into: $(outpath)
@@ -46,7 +46,7 @@ makedocs(
 docs = [
     # We also add JuliaAstro's own generated pages
     MultiDocumenter.MultiDocRef(
-        upstream = joinpath(@__DIR__, outpath),
+        upstream = joinpath(@__DIR__, "build"),
         path = "docs",
         name = "JuliaAstro",
         fix_canonical_url = false,
@@ -58,37 +58,37 @@ docs = [
                 upstream = joinpath(clonedir, "AstroAngles"),
                 path = "AstroAngles",
                 name = "AstroAngles",
-                giturl = "https://github.com/JuliaAstro/AstroAngles.jl",
+                giturl = "https://github.com/JuliaAstro/AstroAngles.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "AstroTime"),
                 path = "AstroTime",
                 name = "AstroTime",
-                giturl = "https://github.com/JuliaAstro/AstroTime.jl",
+                giturl = "https://github.com/JuliaAstro/AstroTime.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "ERFA"),
                 path = "ERFA",
                 name = "ERFA",
-                giturl = "https://github.com/JuliaAstro/ERFA.jl",
+                giturl = "https://github.com/JuliaAstro/ERFA.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "SkyCoords"),
                 path = "SkyCoords",
                 name = "SkyCoords",
-                giturl = "https://github.com/JuliaAstro/SkyCoords.jl",
+                giturl = "https://github.com/JuliaAstro/SkyCoords.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "UnitfulAstro"),
                 path = "UnitfulAstro",
                 name = "UnitfulAstro",
-                giturl = "https://github.com/JuliaAstro/UnitfulAstro.jl",
+                giturl = "https://github.com/JuliaAstro/UnitfulAstro.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "WCS"),
                 path = "WCS",
                 name = "WCS",
-                giturl = "https://github.com/JuliaAstro/WCS.jl",
+                giturl = "https://github.com/JuliaAstro/WCS.jl.git",
             ),
         ]
     ),
@@ -99,31 +99,31 @@ docs = [
                 upstream = joinpath(clonedir, "AstroImages"),
                 path = "AstroImages",
                 name = "AstroImages",
-                giturl = "https://github.com/JuliaAstro/AstroImages.jl",
+                giturl = "https://github.com/JuliaAstro/AstroImages.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "SAOImagesDS9"),
                 path = "SAOImagesDS9",
                 name = "SAOImagesDS9",
-                giturl = "https://github.com/JuliaAstro/SAOImageDS9.jl",
+                giturl = "https://github.com/JuliaAstro/SAOImageDS9.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "Photometry"),
                 path = "Photometry",
                 name = "Photometry",
-                giturl = "https://github.com/juliaastro/Photometry.jl",
+                giturl = "https://github.com/juliaastro/Photometry.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "PSFModels"),
                 path = "PSFModels",
                 name = "PSFModels",
-                giturl = "https://github.com/juliaastro/PSFModels.jl",
+                giturl = "https://github.com/juliaastro/PSFModels.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "LACosmic"),
                 path = "LACosmic",
                 name = "LACosmic",
-                giturl = "https://github.com/juliaastro/LACosmic.jl",
+                giturl = "https://github.com/juliaastro/LACosmic.jl.git",
             ),
         ]
     ),
@@ -134,19 +134,19 @@ docs = [
                 upstream = joinpath(clonedir, "AstroImages"),
                 path = "AstroImages",
                 name = "AstroImages",
-                giturl = "https://github.com/JuliaAstro/AstroImages.jl",
+                giturl = "https://github.com/JuliaAstro/AstroImages.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "FITSIO"),
                 path = "FITSIO",
                 name = "FITSIO",
-                giturl = "https://github.com/JuliaAstro/FITSIO.jl",
+                giturl = "https://github.com/JuliaAstro/FITSIO.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "CFITSIO"),
                 path = "CFITSIO",
                 name = "CFITSIO",
-                giturl = "https://github.com/JuliaAstro/CFITSIO.jl",
+                giturl = "https://github.com/JuliaAstro/CFITSIO.jl.git",
             ),
             MultiDocumenter.Link(
                 "mweastwood/CasaCore",
@@ -173,7 +173,7 @@ docs = [
                 upstream = joinpath(clonedir, "Cosmology"),
                 path = "Cosmology",
                 name = "Cosmology",
-                giturl = "https://github.com/JuliaAstro/Cosmology.jl",
+                giturl = "https://github.com/JuliaAstro/Cosmology.jl.git",
             ),
         ]
     ),
@@ -184,19 +184,19 @@ docs = [
                 upstream = joinpath(clonedir, "Transits"),
                 path = "Transits",
                 name = "Transits",
-                giturl = "https://github.com/JuliaAstro/Transits.jl",
+                giturl = "https://github.com/JuliaAstro/Transits.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "sefffal/PlanetOrbits"),
                 path = "sefffal/PlanetOrbits",
                 name = "sefffal/PlanetOrbits",
-                giturl = "https://github.com/sefffal/PlanetOrbits.jl",
+                giturl = "https://github.com/sefffal/PlanetOrbits.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "EarthOrientation"),
                 path = "EarthOrientation",
                 name = "EarthOrientation",
-                giturl = "https://github.com/JuliaAstro/EarthOrientation.jl",
+                giturl = "https://github.com/JuliaAstro/EarthOrientation.jl.git",
             ),
             MultiDocumenter.Link(
                 "JPLEphemeris",
@@ -206,7 +206,7 @@ docs = [
                 upstream = joinpath(clonedir, "AstroLib"),
                 path = "AstroLib",
                 name = "AstroLib",
-                giturl = "https://github.com/JuliaAstro/AstroLib.jl",
+                giturl = "https://github.com/JuliaAstro/AstroLib.jl.git",
             ),
         ]
     ),
@@ -217,13 +217,13 @@ docs = [
                 upstream = joinpath(clonedir, "BoxLeastSquares"),
                 path = "BoxLeastSquares",
                 name = "BoxLeastSquares",
-                giturl = "https://github.com/JuliaAstro/BoxLeastSquares.jl",
+                giturl = "https://github.com/JuliaAstro/BoxLeastSquares.jl.git",
             ),
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "LombScargle"),
                 path = "LombScargle",
                 name = "LombScargle",
-                giturl = "https://github.com/JuliaAstro/LombScargle.jl",
+                giturl = "https://github.com/JuliaAstro/LombScargle.jl.git",
             ),
         ]
     ),
@@ -243,6 +243,7 @@ docs = [
 MultiDocumenter.make(
     outpath,
     docs;
+    assets_dir = "docs/src/assets",
     search_engine = MultiDocumenter.SearchConfig(
         index_versions = ["stable"],
         engine = MultiDocumenter.FlexSearch
@@ -263,48 +264,46 @@ Downloads.download(
 )
 @info "Final build done"
 
-## TODO: is there a way to pass args to julia-docdeploy?
-#if true
-##if "deploy" in ARGS
-#    @warn "Deploying to GitHub" ARGS
-#    gitroot = normpath(joinpath(@__DIR__, ".."))
-#    run(`git pull`)
-#    outbranch = "master"
-#    has_outbranch = true
-#    if !success(`git checkout $outbranch`)
-#        has_outbranch = false
-#        if !success(`git switch --orphan $outbranch`)
-#            @error "Cannot create new orphaned branch $outbranch."
-#            exit(1)
-#        end
-#    end
-#    for file in readdir(gitroot; join = true)
-#        endswith(file, ".git") && continue
-#        rm(file; force = true, recursive = true)
-#    end
-#    for file in readdir(outpath)
-#        cp(joinpath(outpath, file), joinpath(gitroot, file))
-#    end
-#    run(`git add .`)
-#    if success(`git commit -m 'Aggregate documentation'`)
-#        @info "Pushing updated documentation."
-#        if has_outbranch
-#            run(`git push`)
-#        else
-#            run(`git push -u origin $outbranch`)
-#        end
-#        run(`git checkout main`)
-#    else
-#        @info "No changes to aggregated documentation."
-#    end
-#else
-#    @info "Skipping deployment, 'deploy' not passed. Generated files in docs/out." ARGS
-#    cp(outpath, joinpath(@__DIR__, "out"), force = true)
-#end
+if "deploy" in ARGS
+    @warn "Deploying to GitHub" ARGS
+    gitroot = normpath(joinpath(@__DIR__, ".."))
+    run(`git pull`)
+    outbranch = "master"
+    has_outbranch = true
+    if !success(`git checkout $outbranch`)
+        has_outbranch = false
+        if !success(`git switch --orphan $outbranch`)
+            @error "Cannot create new orphaned branch $outbranch."
+            exit(1)
+        end
+    end
+    for file in readdir(gitroot; join = true)
+        endswith(file, ".git") && continue
+        rm(file; force = true, recursive = true)
+    end
+    for file in readdir(outpath)
+        cp(joinpath(outpath, file), joinpath(gitroot, file))
+    end
+    run(`git add .`)
+    if success(`git commit -m 'Aggregate documentation'`)
+        @info "Pushing updated documentation."
+        if has_outbranch
+            run(`git push`)
+        else
+            run(`git push -u origin $outbranch`)
+        end
+        run(`git checkout source`)
+    else
+        @info "No changes to aggregated documentation."
+    end
+else
+    @info "Skipping deployment, 'deploy' not passed. Generated files in docs/$(outpath)." ARGS
+    cp(outpath, joinpath(@__DIR__, "build"), force = true)
+end
 
-deploydocs(;
-    repo = "github.com/JuliaAstro/JuliaAstro.github.io",
-    push_preview = true,
-    branch = "master",
-    devbranch = "multidocumenter"
-)
+#deploydocs(;
+#    repo = "github.com/JuliaAstro/JuliaAstro.github.io",
+#    push_preview = true,
+#    branch = "master",
+#    devbranch = "source"
+#)
