2efcffeb7d758bc75ae2e5eabc6a4396e04853f0
icweaver <weaveric@gmail.com>
2025-04-04 07:20:17 -0700

    try deploydocs directly

--- docs/make.jl
+++ docs/make.jl
@@ -263,48 +263,48 @@ Downloads.download(
 )
 @info "Final build done"
 
-# TODO: is there a way to pass args to julia-docdeploy?
-if true
-#if "deploy" in ARGS
-    @warn "Deploying to GitHub" ARGS
-    gitroot = normpath(joinpath(@__DIR__, ".."))
-    run(`git pull`)
-    outbranch = "master"
-    has_outbranch = true
-    if !success(`git checkout $outbranch`)
-        has_outbranch = false
-        if !success(`git switch --orphan $outbranch`)
-            @error "Cannot create new orphaned branch $outbranch."
-            exit(1)
-        end
-    end
-    for file in readdir(gitroot; join = true)
-        endswith(file, ".git") && continue
-        rm(file; force = true, recursive = true)
-    end
-    for file in readdir(outpath)
-        cp(joinpath(outpath, file), joinpath(gitroot, file))
-    end
-    run(`git add .`)
-    if success(`git commit -m 'Aggregate documentation'`)
-        @info "Pushing updated documentation."
-        if has_outbranch
-            run(`git push`)
-        else
-            run(`git push -u origin $outbranch`)
-        end
-        run(`git checkout main`)
-    else
-        @info "No changes to aggregated documentation."
-    end
-else
-    @info "Skipping deployment, 'deploy' not passed. Generated files in docs/out." ARGS
-    cp(outpath, joinpath(@__DIR__, "out"), force = true)
-end
+## TODO: is there a way to pass args to julia-docdeploy?
+#if true
+##if "deploy" in ARGS
+#    @warn "Deploying to GitHub" ARGS
+#    gitroot = normpath(joinpath(@__DIR__, ".."))
+#    run(`git pull`)
+#    outbranch = "master"
+#    has_outbranch = true
+#    if !success(`git checkout $outbranch`)
+#        has_outbranch = false
+#        if !success(`git switch --orphan $outbranch`)
+#            @error "Cannot create new orphaned branch $outbranch."
+#            exit(1)
+#        end
+#    end
+#    for file in readdir(gitroot; join = true)
+#        endswith(file, ".git") && continue
+#        rm(file; force = true, recursive = true)
+#    end
+#    for file in readdir(outpath)
+#        cp(joinpath(outpath, file), joinpath(gitroot, file))
+#    end
+#    run(`git add .`)
+#    if success(`git commit -m 'Aggregate documentation'`)
+#        @info "Pushing updated documentation."
+#        if has_outbranch
+#            run(`git push`)
+#        else
+#            run(`git push -u origin $outbranch`)
+#        end
+#        run(`git checkout main`)
+#    else
+#        @info "No changes to aggregated documentation."
+#    end
+#else
+#    @info "Skipping deployment, 'deploy' not passed. Generated files in docs/out." ARGS
+#    cp(outpath, joinpath(@__DIR__, "out"), force = true)
+#end
 
-#deploydocs(;
-#    repo = "github.com/JuliaAstro/JuliaAstro.github.io",
-#    push_preview = true,
-#    branch = "master",
-#    devbranch = "source"
-#)
+deploydocs(;
+    repo = "github.com/JuliaAstro/JuliaAstro.github.io",
+    push_preview = true,
+    branch = "master",
+    devbranch = "multidocumenter"
+)
