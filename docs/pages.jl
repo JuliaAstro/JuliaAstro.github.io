8f701f9c2d80d14da3b82d1f3d56e974be7d7e37
icweaver <weaveric@gmail.com>
2025-04-06 21:22:58 -0700

    simplify sidebar

--- docs/pages.jl
+++ docs/pages.jl
@@ -3,7 +3,7 @@
 # Plain documenter pages
 fullpages = Any[
     "Home"=>"index.md",
-    # "Ecosystem" => "ecosystem.md";
+    "Ecosystem" => "ecosystem.md",
     "Tutorials" => [
         "tutorials/index.md",
         "General" => [
@@ -12,11 +12,11 @@ fullpages = Any[
             "tutorials/curve-fit.md",
         ]
     ],
-    "Time, Coordinates, & Units" => "highlevels/timecoords.md",
-    "Images" => "highlevels/images.md",
-    "Data I/O" => "highlevels/dataio.md",
-    "Cosmology" => "highlevels/cosmology.md",
-    "Orbits & Emphemerides" => "highlevels/orb-ephem.md",
-    "Numerical Utilities" => "highlevels/numerical-utils.md",
-    "Statistics" => "highlevels/stats.md",
+    #"Time, Coordinates, & Units" => "highlevels/timecoords.md",
+    #"Images" => "highlevels/images.md",
+    #"Data I/O" => "highlevels/dataio.md",
+    #"Cosmology" => "highlevels/cosmology.md",
+    #"Orbits & Emphemerides" => "highlevels/orb-ephem.md",
+    #"Numerical Utilities" => "highlevels/numerical-utils.md",
+    #"Statistics" => "highlevels/stats.md",
 ]
