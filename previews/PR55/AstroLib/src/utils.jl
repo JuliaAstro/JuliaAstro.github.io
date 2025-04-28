538420f501eadee9532f48125c5d7425d22d4cbb
Documenter.jl <documenter@juliadocs.github.io>
2025-04-28 22:09:10 +0000

    build based on 448f6e9

---
+++ previews/PR55/AstroLib/src/utils.jl
@@ -0,0 +1,164 @@
+# This file is a part of AstroLib.jl. License is MIT "Expat".
+# Copyright (C) 2016 Mosè Giordano.
+
+include("adstring.jl")
+export adstring
+
+include("airtovac.jl")
+export airtovac
+
+include("aitoff.jl")
+export aitoff
+
+include("altaz2hadec.jl")
+export altaz2hadec
+
+include("bprecess.jl")
+export bprecess
+
+include("calz_unred.jl")
+export calz_unred
+
+include("ct2lst.jl")
+export ct2lst
+
+include("daycnv.jl")
+export daycnv
+
+include("deredd.jl")
+export deredd
+
+include("eci2geo.jl")
+export eci2geo
+
+include("eqpole.jl")
+export eqpole
+
+include("flux2mag.jl")
+export flux2mag
+
+include("gal_uvw.jl")
+export gal_uvw
+
+include("geo2eci.jl")
+export geo2eci
+
+include("geo2geodetic.jl")
+export geo2geodetic
+
+include("geo2mag.jl")
+export geo2mag
+
+include("geodetic2geo.jl")
+export geodetic2geo
+
+include("get_date.jl")
+export get_date
+
+include("get_juldate.jl")
+export get_juldate
+
+include("gcirc.jl")
+export gcirc
+
+include("hadec2altaz.jl")
+export hadec2altaz
+
+include("helio_jd.jl")
+export helio_jd
+
+include("helio_rv.jl")
+export helio_rv
+
+include("jdcnv.jl")
+export jdcnv
+
+include("jprecess.jl")
+export jprecess
+
+include("juldate.jl")
+export juldate
+
+include("kepler_solver.jl")
+export kepler_solver
+
+include("lsf_rotate.jl")
+export lsf_rotate
+
+include("mag2flux.jl")
+export mag2flux
+
+include("mag2geo.jl")
+export mag2geo
+
+include("month_cnv.jl")
+export month_cnv
+
+include("moonpos.jl")
+export moonpos
+
+include("mphase.jl")
+export mphase
+
+include("nutate.jl")
+export nutate
+
+include("paczynski.jl")
+export paczynski
+
+include("planck_freq.jl")
+export planck_freq
+
+include("planck_wave.jl")
+export planck_wave
+
+include("polrec.jl")
+export polrec
+
+include("posang.jl")
+export posang
+
+include("precess.jl")
+export precess
+
+include("precess_xyz.jl")
+export precess_xyz
+
+include("premat.jl")
+export premat
+
+include("radec.jl")
+export radec
+
+include("recpol.jl")
+export recpol
+
+include("rhotheta.jl")
+export rhotheta
+
+include("sixty.jl")
+export sixty
+
+include("sphdist.jl")
+export sphdist
+
+include("sunpos.jl")
+export sunpos
+
+include("ten.jl")
+export ten, tenv
+
+include("trueanom.jl")
+export trueanom
+
+include("vactoair.jl")
+export vactoair
+
+include("xyz.jl")
+export xyz
+
+include("ydn2md.jl")
+export ydn2md
+
+include("ymd2dn.jl")
+export ymd2dn
