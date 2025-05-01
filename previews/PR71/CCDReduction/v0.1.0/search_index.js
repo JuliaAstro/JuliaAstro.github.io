c9f8d010069a9d16e8764dc42e5393e805b7bd67
Documenter.jl <documenter@juliadocs.github.io>
2025-05-01 09:27:39 +0000

    build based on 9babaa9

---
+++ previews/PR71/CCDReduction/v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CCDReduction.jl-1","page":"Home","title":"CCDReduction.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"This package will provide tools for basic data reductions of CCD images.","category":"page"}]
+}
