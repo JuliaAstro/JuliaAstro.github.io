95ed08fbe6f9ab6cc8d1ebb37fa85a6af99d5eff
Ian Weaver <weaveric@gmail.com>
2025-07-09 05:28:38 -0700

    Delete previews directory

--- previews/PR122/SAOImageDS9/versions.js
+++
@@ -1,6 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v0.2",
-];
-var DOCUMENTER_NEWEST = "v0.2.0";
-var DOCUMENTER_STABLE = "stable";
--- previews/PR124/WCS/versions.js
+++
@@ -1,7 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v0.6",
-  "v0.5",
-];
-var DOCUMENTER_NEWEST = "v0.6.2";
-var DOCUMENTER_STABLE = "stable";
