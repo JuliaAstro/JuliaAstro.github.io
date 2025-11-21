d5730ae5a5c4211c01f83d959bf3b251917c5829
Ian Weaver <weaveric@gmail.com>
2025-11-21 15:33:35 -0800

    Delete previews directory

--- previews/PR179/CCDReduction/v0.1.0/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#CCDReduction.jl-1","page":"Home","title":"CCDReduction.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"This package will provide tools for basic data reductions of CCD images.","category":"page"}]
-}
