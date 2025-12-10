8612975a1ba57d2a8b7cd8944ff1f6612c98b008
Documenter.jl <documenter@juliadocs.github.io>
2025-12-10 03:45:12 +0000

    build based on 8cb6549

--- SolarPosition/versions.js
+++ SolarPosition/versions.js
@@ -1,7 +1,9 @@
 var DOC_VERSIONS = [
   "stable",
+  "v3.0",
+  "v0.3",
   "v0.2",
   "v0.1",
 ];
-var DOCUMENTER_NEWEST = "v0.2.3";
+var DOCUMENTER_NEWEST = "v3.0.0";
 var DOCUMENTER_STABLE = "stable";
