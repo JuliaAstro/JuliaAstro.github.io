726a41c7e40ba96db48076a35292fd1fdc6c2770
Documenter.jl <documenter@juliadocs.github.io>
2026-09-06 07:14:34 +0000

    build based on e47d76d

--- SOFA/versions.js
+++ SOFA/versions.js
@@ -1,10 +1,11 @@
 var DOC_VERSIONS = [
   "stable",
+  "v2.0",
   "v1.5",
   "v1.4",
   "v1.3",
   "v1.2",
   "v1.1",
 ];
-var DOCUMENTER_NEWEST = "v1.5.0";
+var DOCUMENTER_NEWEST = "v2.0.0";
 var DOCUMENTER_STABLE = "stable";
